0cbd347efd64bb3c049c4df22eb0e22b48352baf
goldengua <lijiaxuan1111@gmail.com>
2020-10-06 00:24:51 -0500

    Update main.js

--- data_includes/main.js
+++ data_includes/main.js
@@ -1,5 +1,5 @@
 PennController.ResetPrefix(null); // Initiates PennController
-AddHost("https://github.com/goldengua/TimedPictureSelection/tree/master/chunk_includes/");
+AddHost("https://github.com/apex-lab/TimedPictureSelection/tree/master/chunk_includes/");
 // Start typing your code here
  // Initiates PennController
 
